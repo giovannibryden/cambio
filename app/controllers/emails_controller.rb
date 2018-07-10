6069fbc1e2dcd3744df23ea0fd3208ac1ed27c30
giovannibryden@gmail.com <giovannibryden@Gios-MacBook-Pro.local>
2018-07-09 22:07:03 -0700

    More db work.

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,21 @@
+class EmailsController < ApplicationController
+	def new
+		@email = Email.new
+	end
+
+	def create
+		@email = Email.new(email_params)
+		if @email.save
+			flash[:notice] = 'Success!'
+		else
+			flash[:notice] = 'Something went wrong.'
+			render 'root'
+		end
+	end
+end
+
+private
+
+	def email_params
+		params.require(:email).permit(:email)
+	end
